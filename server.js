55f08a065ef035a2ab2def4adf80b74f50a9057e
Omer Levi Hevroni <omerlh@gmail.com>
2018-11-04 22:53:17 +0200

    print only token

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = new http.Server();
 
 app.on('request', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write(JSON.stringify(req.headers));
+  res.write(JSON.stringify(req.headers["x-ms-token-aad-access-token"]));
   res.write('Hello World');
   res.end('\n');
 });
