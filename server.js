0ea7691716c0e14e112db321d67b48bd6e9c3f5a
Omer Levi Hevroni <omerlh@gmail.com>
2018-11-04 23:29:13 +0200

    return header in the requested format

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const port = process.env.PORT || 1337;
 const app = new http.Server();
 
 app.on('request', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write(req.headers["x-ms-token-aad-id-token"]);
+  res.writeHead(200, 
+    { 
+      'Content-Type': 'text/plain',
+      'Authorization': `Bearer req.headers["x-ms-token-aad-id-token"]`
+    }
+    );
+  res.write('ok');
   res.end('\n');
 });
 
