01069fc88a57b214848a9a3ecd7c5a70372580c1
Omer Levi Hevroni <omerlh@gmail.com>
2018-11-05 08:49:52 +0200

    now actually return the token

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const port = process.env.PORT || 1337;
 const app = new http.Server();
 
 app.on('request', (req, res) => {
+
+  const token = req.headers["x-ms-token-aad-id-token"];
+
   res.writeHead(200, 
     { 
       'Content-Type': 'text/plain',
-      'Authorization': `Bearer req.headers["x-ms-token-aad-id-token"]`
+      'Authorization': `Bearer ${token}`
     }
     );
   res.write('ok');
